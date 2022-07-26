e31fdc32dfb02ad21ea9e6af57453c6b8dd86720
Stephanie Roe <stephanie.j.roe@gmail.com>
2022-07-26 11:25:54 -0600

    Clean up Boilerplate code and fetch data from API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [allArticles, setAllArticles] = useState([])
+
+  useEffect(() => {
+    const url = "https://api.nytimes.com/svc/topstories/v2/home.json?api-key=M6ABMiMVTvWgQutZetmU3W17rZz3HBge"
+
+    const fetchArticles = async () => {
+      try {
+        const response = await fetch(url)
+        const data = await response.json()
+        setAllArticles(data.results)
+      } catch (error) {
+        console.log("error:", error)
+      }
+    }
+
+    fetchArticles()
+  }, [])
 }
 
 export default App;
